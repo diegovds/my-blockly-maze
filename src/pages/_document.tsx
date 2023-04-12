6e6cb4c743278321febcea73dd3500393a6cbe21
Diego Viana <dsantos@inf.ufsm.br>
2023-04-12 17:50:21 -0300

    changing to next/babel

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,14 @@
-import React from "react";
 import Document, {
-  DocumentInitialProps,
-  DocumentContext,
   Html,
   Head,
   Main,
   NextScript,
+  DocumentContext,
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
@@ -38,9 +34,9 @@ export default class MyDocument extends Document {
     }
   }
 
-  render(): JSX.Element {
+  render() {
     return (
-      <Html lang="pt">
+      <Html lang="pt-BR">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Fredoka&display=swap"
