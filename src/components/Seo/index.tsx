d701a3503267994bff9e7b800d7197d890bf3bf0
Diego Viana <dsantos@inf.ufsm.br>
2023-04-12 16:23:28 -0300

    create Seo component

---
+++ src/components/Seo/index.tsx
@@ -0,0 +1,38 @@
+import Head from "next/head";
+
+type Props = {
+  title: string;
+  description: string;
+  image: string;
+};
+
+const Seo = ({ title, description, image }: Props) => {
+  return (
+    <Head>
+      {/* Standard metadata tags */}
+      <title>{title}</title>
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, user-scalable=no"
+      />
+      {/* End standard metadata tags */}
+      {/* Facebook tags */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={`https://${window.location.hostname}`} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      {/* End Facebook tags */}
+      {/* Twitter tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+      {/* End Twitter tags */}
+    </Head>
+  );
+};
+
+export default Seo;
