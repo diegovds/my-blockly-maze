e8dd0cafcc3c337862eee6e3e7b042c16952ebca
Diego Viana <dsantos@inf.ufsm.br>
2023-04-12 20:17:03 -0300

    update layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,25 +2,18 @@ import { ReactElement } from "react";
 import styles from "./Layout.module.css";
 
 import Navbar from "../Navbar";
-import { useSession } from "next-auth/react";
 
 type Props = {
   children: ReactElement;
 };
 
 const Layout = ({ children }: Props) => {
-  const { status: sessionStatus } = useSession();
-
   return (
     <div>
-      {sessionStatus !== "loading" && (
-        <>
-          <header>
-            <Navbar />
-          </header>
-          <main className={styles.main}>{children}</main>
-        </>
-      )}
+      <header>
+        <Navbar />
+      </header>
+      <main className={styles.main}>{children}</main>
     </div>
   );
 };
