e272b1f97c0ea40e8c9355a71185e35418301508
Diego Viana <dsantos@inf.ufsm.br>
2023-04-12 16:24:10 -0300

    add Seo component in home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,16 @@
 import Head from "next/head";
 
 import * as C from "@/styles/Home.styles";
+import Seo from "@/components/Seo";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>My Blockly Maze | Home</title>
-        <meta
-          name="description"
-          content="Página Home da plataforma My Blockly Maze"
-        />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, user-scalable=no"
-        />
-      </Head>
+      <Seo
+        title="My Blockly Maze | Home"
+        description="Página Home da plataforma My Blockly Maze"
+        image=""
+      />
       <C.Container>
         <p>My Blockly Maze</p>
       </C.Container>
