fa797d00c9adb2eead3bac42413cb216dd531e0c
Diego Viana <dsantos@inf.ufsm.br>
2023-04-14 16:39:52 -0300

    creating Iframe component

---
+++ src/components/Iframe/index.tsx
@@ -0,0 +1,54 @@
+import { useCallback, useEffect, useRef } from "react";
+import * as C from "./styles";
+
+type Props = {
+  link: string;
+  redirect: (mazeId?: string) => void;
+};
+
+const Iframe = ({ link, redirect }: Props) => {
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
+  const handleMessage = useCallback(
+    (e: MessageEvent) => {
+      // para ter acesso ao dado temos que acessar a propriedade data
+      //if (e.data === "mensagem vinda do iframe") {
+      if (String(e.data).includes("mensagem")) {
+        //console.log(e.data);
+        if (String(e.data).includes("mazeId=")) {
+          //console.log("tem mazeId");
+          let mazeId = String(e.data).slice(17);
+          //console.log(mazeId); /** id do novo maze criado */
+          redirect(mazeId);
+        } else {
+          redirect();
+        }
+      }
+    },
+    [redirect]
+  );
+
+  useEffect(() => {
+    // window escutando o evento `message` que o postMessage envia
+    window.addEventListener("message", handleMessage);
+    // 👇️ remove o event listener quando o componente é desmontado
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, [handleMessage]);
+
+  return (
+    <C.Container>
+      <C.Iframe
+        src={link}
+        title="iframeLink"
+        frameBorder="0"
+        allowFullScreen={true}
+        ref={iframeRef}
+        onLoad={() => iframeRef.current?.scrollIntoView()}
+      />
+    </C.Container>
+  );
+};
+
+export default Iframe;
---
+++ src/components/Iframe/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin-top: 2rem;
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  padding-top: 56.25%; /* 16:9 Aspect Ratio (divide 9 by 16 = 0.5625) */
+  /* padding-bottom: 45%; */
+`;
+
+export const Iframe = styled.iframe`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 92%;
+`;
