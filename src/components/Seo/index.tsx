1666f7683de14b071b8708d280b284ef4e51a2aa
Diego Viana <dsantos@inf.ufsm.br>
2023-04-19 17:48:56 -0300

    finxing metatag viewport

--- src/components/Seo/index.tsx
+++ src/components/Seo/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { NextSeo } from "next-seo";
 
 type Props = {
@@ -9,26 +10,37 @@ type Props = {
 
 const Seo = ({ title, description, image, path }: Props) => {
   return (
-    <NextSeo
-      title={title}
-      description={description}
-      canonical="https://my-blockly-maze.vercel.app"
-      openGraph={{
-        url: `https://my-blockly-maze.vercel.app${path}`,
-        title: title,
-        description: description,
-        images: [
-          {
-            url: image ? image : "https://i.imgur.com/C2xpI35.png",
-          },
-        ],
-        siteName: "My BLOCKLY Maze",
-        type: "website",
-      }}
-      twitter={{
-        cardType: "summary_large_image",
-      }}
-    />
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=no"
+        />
+        <link rel="icon" href="/puzzle-icon.png" />
+        <link rel="apple-touch-icon" href="/puzzle-icon.png" />
+        <meta name="theme-color" content="#add8e6" />
+      </Head>
+      <NextSeo
+        title={title}
+        description={description}
+        canonical="https://my-blockly-maze.vercel.app"
+        openGraph={{
+          url: `https://my-blockly-maze.vercel.app${path}`,
+          title: title,
+          description: description,
+          images: [
+            {
+              url: image ? image : "https://i.imgur.com/C2xpI35.png",
+            },
+          ],
+          siteName: "My BLOCKLY Maze",
+          type: "website",
+        }}
+        twitter={{
+          cardType: "summary_large_image",
+        }}
+      />
+    </>
   );
 };
 
