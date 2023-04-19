165e5118afb5dafda80b7459bfd3fb848fa54486
Diego Viana <dsantos@inf.ufsm.br>
2023-04-19 11:27:54 -0300

    handling page loading

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,22 +1,36 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import * as C from "./styles";
 
 import Navbar from "../Navbar";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 type Props = {
   children: ReactElement;
 };
 
 const Layout = ({ children }: Props) => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
   const { status: sessionStatus } = useSession();
 
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => setLoading(true));
+    router.events.on("routeChangeComplete", () => setLoading(false));
+    router.events.on("routeChangeError", () => setLoading(false));
+    return () => {
+      router.events.off("routeChangeStart", () => setLoading(true));
+      router.events.off("routeChangeComplete", () => setLoading(false));
+      router.events.off("routeChangeError", () => setLoading(false));
+    };
+  }, [router.events]);
+
   return (
-    <C.Container loading={sessionStatus === "loading" ? 'hidden' : 'visible'}>
+    <C.Container loading={sessionStatus === "loading" ? "hidden" : "visible"}>
       <header>
         <Navbar />
       </header>
-      <C.Main>{children}</C.Main>
+      <C.Main loading={loading ? "hidden" : "visible"}>{children}</C.Main>
     </C.Container>
   );
 };
--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -1,15 +1,18 @@
 import styled from "styled-components";
 
-type ContainerProps = {
+type Props = {
   loading: string;
 };
 
-export const Container = styled.div<ContainerProps>`
+export const Container = styled.div<Props>`
   visibility: ${(props) => props.loading};
   opacity: ${(props) => (props.loading === "hidden" ? 0 : 1)};
   transition: all 0.3s ease;
 `;
 
-export const Main = styled.main`
+export const Main = styled.main<Props>`
+  visibility: ${(props) => props.loading};
+  opacity: ${(props) => (props.loading === "hidden" ? 0 : 1)};
+  transition: all 0.3s ease;
   min-height: calc(100vh - 70.94px); /*  70.94px é a altura da navbar */
 `;
