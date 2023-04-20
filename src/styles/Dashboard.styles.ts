58777618c59be26d1f0ae77e0ea89a48f2c03920
Diego Viana <dsantos@inf.ufsm.br>
2023-04-20 17:37:55 -0300

    Update Dashboard.styles.ts

--- src/styles/Dashboard.styles.ts
+++ src/styles/Dashboard.styles.ts
@@ -6,4 +6,8 @@ export const NoMazes = styled.div`
   justify-content: center;
   align-items: center;
   min-height: calc(50vh - 70.94px); /*  70.94px é a altura da navbar */
+
+  p {
+    color: #aaa;
+  }
 `;
