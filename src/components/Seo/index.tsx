2bf70b84ef714e3da69f77b3a795604192e651f9
Diego Viana <dsantos@inf.ufsm.br>
2023-04-21 20:58:40 -0300

    add new favicon

--- src/components/Seo/index.tsx
+++ src/components/Seo/index.tsx
@@ -16,8 +16,24 @@ const Seo = ({ title, description, image, path }: Props) => {
           name="viewport"
           content="width=device-width, initial-scale=1, user-scalable=no"
         />
-        <link rel="icon" href="/puzzle-icon.png" />
-        <link rel="apple-touch-icon" href="/puzzle-icon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#add8e6" />
       </Head>
       <NextSeo
