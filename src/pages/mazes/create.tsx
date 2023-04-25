1ce7f3688b465a21ef67152cb68bc645024c0be7
Diego Viana <dsantos@inf.ufsm.br>
2023-04-24 21:58:26 -0300

    checking screen width before running games builder

--- src/pages/mazes/create.tsx
+++ src/pages/mazes/create.tsx
@@ -5,6 +5,11 @@ import { User } from "@/types/User";
 import { useRouter } from "next/router";
 import Iframe from "@/components/Iframe";
 import Seo from "@/components/Seo";
+import { useIsFirstRender, useMediaQuery } from "usehooks-ts";
+import * as C from "@/styles/Create.styles";
+import { useState, useEffect } from "react";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, ToastOptions, toast } from "react-toastify";
 
 type Props = {
   user: User;
@@ -12,6 +17,36 @@ type Props = {
 
 const Create = ({ user: { id } }: Props) => {
   const router = useRouter();
+  const [mobile, setMobile] = useState(false);
+  const isMobile = useMediaQuery("(max-width: 1115px)");
+  const isFirst = useIsFirstRender();
+
+  useEffect(() => {
+    const toastConfig: ToastOptions<{}> = {
+      position: "top-left",
+      closeButton: false,
+      hideProgressBar: true,
+      closeOnClick: false,
+      pauseOnHover: false,
+      draggable: false,
+      theme: "colored",
+    };
+
+    const notify = (message: string) => {
+      toast.error(message, {
+        autoClose: 3000,
+        ...toastConfig,
+      });
+    };
+
+    if (isMobile && !isFirst) {
+      notify(
+        "A execução do jogo não está disponível para essa largura de tela."
+      );
+    }
+
+    setMobile(isMobile);
+  }, [isMobile, isFirst]);
 
   const redirect = (mazeId?: string) => {
     router.push(`/mazes/${mazeId}`);
@@ -24,10 +59,15 @@ const Create = ({ user: { id } }: Props) => {
         description={`Página de criação dos jogos de labirinto da plataforma My BLOCKLY Maze.`}
         path="/mazes/create"
       />
-      <Iframe
-        link={`https://maze-game-builder-v2.vercel.app/index.html?userId=${id}`}
-        redirect={redirect}
-      />
+      <>
+        <C.Container hidden={mobile}>
+          <Iframe
+            link={`https://maze-game-builder-v2.vercel.app/index.html?userId=${id}`}
+            redirect={redirect}
+          />
+        </C.Container>
+        <ToastContainer />
+      </>
     </>
   );
 };
