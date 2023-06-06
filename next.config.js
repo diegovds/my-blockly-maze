d00c3ba0f927615eb40e322179a4768f2315822e
Diego Viana <dsantos@inf.ufsm.br>
2023-06-06 01:02:26 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,9 +9,17 @@ const nextConfig = {
       {
         source: "/api/:path*",
         headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Headers", value: "*" },
-          { key: "Access-Control-Request-Headers", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "POST, PUT, PATCH, GET, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization",
+          },
         ],
       },
     ];
