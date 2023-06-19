9976377f91cde031221400bb3b86113b8d6bfb11
Diego Viana <dsantos@inf.ufsm.br>
2023-06-19 14:50:19 -0300

    adjusting date conversion

--- src/libs/dayjs.ts
+++ src/libs/dayjs.ts
@@ -1,12 +1,19 @@
 import dayjs from "dayjs";
 import ptBr from "dayjs/locale/pt-br";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
 
 dayjs.locale(ptBr);
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export const fullDateFormatting = (date: Date): string => {
-  return dayjs(date).format("D[ de ]MMMM[ de ]YYYY");
+  return dayjs
+    .utc(date)
+    .tz("America/Sao_Paulo")
+    .format("D[ de ]MMMM[ de ]YYYY");
 };
 
 export const compactDateFormatting = (date: Date): string => {
-  return dayjs(date).format("DD/MM/YYYY");
+  return dayjs.utc(date).tz("America/Sao_Paulo").format("DD/MM/YYYY");
 };
