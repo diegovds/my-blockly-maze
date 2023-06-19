2ba70aa1e86390d70963a68090591b46835039ae
Diego Viana <dsantos@inf.ufsm.br>
2023-06-19 14:49:25 -0300

    removing delay to show loading screen

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,7 +4,6 @@ import * as C from "./styles";
 import Navbar from "../Navbar";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import { useInterval } from "usehooks-ts";
 import Loading from "../Loading";
 
 type Props = {
@@ -16,42 +15,26 @@ const Layout = ({ children }: Props) => {
   const [loading, setLoading] = useState(false);
   const [hidden, setHidden] = useState(false);
   const { status: sessionStatus } = useSession();
-  const [timeElapsed, setTimeElapsed] = useState<number>(0);
-
-  useInterval(
-    () => {
-      // Your custom logic here
-      setTimeElapsed(timeElapsed + 1);
-    },
-    // Delay in milliseconds or null to stop it
-    loading ? 1000 : null
-  );
 
   useEffect(() => {
     router.events.on("routeChangeStart", () => {
       setLoading(true);
-      setTimeElapsed(0);
     });
     router.events.on("routeChangeComplete", () => {
       setLoading(false);
-      setTimeElapsed(0);
     });
     router.events.on("routeChangeError", () => {
       setLoading(false);
-      setTimeElapsed(0);
     });
     return () => {
       router.events.off("routeChangeStart", () => {
         setLoading(true);
-        setTimeElapsed(0);
       });
       router.events.off("routeChangeComplete", () => {
         setLoading(false);
-        setTimeElapsed(0);
       });
       router.events.off("routeChangeError", () => {
         setLoading(false);
-        setTimeElapsed(0);
       });
     };
   }, [router.events]);
@@ -64,7 +47,7 @@ const Layout = ({ children }: Props) => {
     <C.Container
       positionContainer={hidden}
       load={sessionStatus === "loading" ? true : false}
-      timeElapsed={timeElapsed > 0 ? true : false}
+      timeElapsed={loading ? true : false}
     >
       <header>
         <Navbar openMenu={openMenu} />
@@ -72,7 +55,7 @@ const Layout = ({ children }: Props) => {
       <C.Main load={loading && router.pathname !== "/search" ? true : false}>
         {children}
       </C.Main>
-      {timeElapsed > 0 && <Loading />}
+      {loading && <Loading />}
     </C.Container>
   );
 };
